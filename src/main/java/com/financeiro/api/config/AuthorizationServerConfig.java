f68e8955e86e71867ee27843fc3ecc1b81dcf26e
Jaqueline <jaquelinesv90@gmail.com>
2020-06-18 17:26:39 -0300

    refazendo a implementação do oauth2

---
+++ src/main/java/com/financeiro/api/config/AuthorizationServerConfig.java
@@ -0,0 +1,21 @@
+package com.financeiro.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+
+	@Autowired	
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(ClienteDetailsServiceConfigurer clients) throws Exception {
+		
+	}
+	
+}
