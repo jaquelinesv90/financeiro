915594a6294a7f3cecb76abacb423c30578436ed
Jaqueline <jaquelinesv90@gmail.com>
2021-03-03 22:16:08 -0300

    criação do recurso de categoria

--- src/main/java/resource/CategoriaResource.java
+++ src/main/java/resource/CategoriaResource.java
@@ -1,5 +1,33 @@
 package resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Categoria;
+import repository.CategoriaRepository;
+
+/* no rest trabalhamos em cima de recursos
+ * é a classe que vai expor tudo que esteja relacionado
+ * com categoria.
+ * @RestController - controlador,uma classe que recebe requisições o retorno será
+ * convertido para json.não é necessário adicionar anotações extras nos nossos métodos.
+ * @RequestMapping - mapeamento da requisição, quando chegar uma requisição ele identifica 
+ * pela url e direciona.
+ */
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public List<Categoria> listar(){
+		return repository.findAll();
+	}
 
 }
